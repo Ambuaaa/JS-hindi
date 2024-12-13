9a9fcd9ef155edad608ba65daaa43b588264cfe3
Abhishek Kumar <105266096+Ambuaaa@users.noreply.github.com>
2024-12-13 20:03:14 +0000

    2nd day

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3 + 3) we are usoing nodejs , not browser 
+
+console.log(3 +
+    3) // code readiblity should be high 
+
+console.log("Abhishek")
+
+let name = "abhishek"
+let age = 22
+let isLogged = true 
+let temperatue = null
+
+/*
+Datatypes
+number => 2 to power 53 
+bin int
+string => ""
+boolean => true/ false 
+null => standalone value
+undefined
+symbol => unique
+object 
+*/
+
+console.log(typeof name);
+console.log(typeof undefined); // gives the type - undefined
+console.log(typeof null); // gives the type - object
+
---
+++ 01_basics/03conversionOperation.js
@@ -0,0 +1,94 @@
+let score = "abhishek" 
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLogged = "abhishek"
+let booleanisLogged = Boolean(isLogged)
+//console.log(booleanisLogged);
+
+// 1 => true , 0 => false 
+// "" => false 
+// "abhishek" = true 
+
+let num = 33
+let stringNum = String(num)
+//console.log(stringNum)
+//console.log(typeof stringNum);
+
+// ******* Operation ********
+
+let value = 3
+let negval = -value
+console.log(negval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " abhishek"
+
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2"); 
+// console.log("1"+ 2 + 2); // If string first then all is treated as string
+// console.log( 1 + 2 + "2"); // If string last , then the operation is already done 
+
+// Don't be depended on this type of code . Use parenthesis . Code readibility is the highest 
+
+console.log(true); // 0 is the answer
+console.log(+true); // 1 is the answer
+console.log(+""); // 0 is the answer
+console.log(+ undefined); // NaN 
+
+// prefix and postfix increment
+
+// let gamecounter = 100
+// gamecounter++ ;
+// console.log(gamecounter); // 101
+
+// let gamecounter2 = 100
+// ++gamecounter2 ;
+// console.log(gamecounter2); // 101
+
+// // mdn
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
